b78ea68511cb555d5b3d26de0513c846638ff4ae
Akshat-developer14 <codeprogramme123@gmail.com>
2024-11-04 19:53:37 +0530

    Changing email name

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,11 +2,12 @@ import {Resend} from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
+const ourEmail = "authentication@secure-app.shop";
 
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
     await resend.emails.send({
-        from: 'NextjsAuthentication@resend.dev',
+        from: ourEmail,
       to: email,
       subject: '2FA Token',
       html: `<p>your 2FA token is ${token}.</p>`,
@@ -17,7 +18,7 @@ export const sendTwoFactorEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
-      from: 'NextjsAuthentication@resend.dev',
+      from: ourEmail,
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -27,7 +28,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
     await resend.emails.send({
-        from: 'NextjsAuthentication@resend.dev',
+        from: ourEmail,
       to: email,
       subject: 'Email Verification',
       html: `<p>Click <a href="${confirmLink}">here</a> to verify your email.</p>`,
